9d6f781589f270a546384781e6f2ebc1802f9daa
badl0se <jakegoodwin110@gmail.com>
2024-09-05 18:25:12 -0400

    updated commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div>
+      <p>IT'S TIME TO GET</p>
+      <h1>Buff</h1>
+      <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+    </div>
   )
 }
